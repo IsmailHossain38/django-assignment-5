d2713eafd6e2ebb54aace40b5c653cbdd140dc01
IsmailHossain38 <ismailhossains383@gmail.com>
2024-01-06 23:49:31 +0600

    some changes

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-# CSRF_TRUSTED_ORIGINS = ['https://library-management-ws7t.onrender.com','https://*.127.0.0.1']
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://librarymanagement-j63g.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
