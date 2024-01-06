d5f90e282597258929102a32ee235c8e525b6051
IsmailHossain38 <ismailhossains383@gmail.com>
2024-01-06 19:31:14 +0600

    csrf added

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://library-management-ws7t.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
@@ -132,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-ALLOWED_HOSTS =["*"]
+
 
 
 # Base url to serve media files
